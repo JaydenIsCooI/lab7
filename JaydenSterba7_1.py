e9dc7e8753b490d69dfcf2d5207f17c41fa0d3c4
jayden <jaydensterba@gmail.com>
2023-03-10 17:17:46 -0600

    Initial commit

---
+++ JaydenSterba7_1.py
@@ -0,0 +1,24 @@
+def power(x, y):
+
+    if y == 0:
+        return 1
+    else:
+        return x * power(x, y - 1)
+
+
+def cat_ears(n):
+
+    if n == 0:
+        return 0
+    else:
+        return 2 + cat_ears(n - 1)
+
+
+def alien_ears(n):
+
+    if n == 0:
+        return 0
+    elif n % 2 == 1:
+        return 3 + alien_ears(n - 1)
+    elif n % 2 == 0:
+        return 2 + alien_ears(n - 1)
